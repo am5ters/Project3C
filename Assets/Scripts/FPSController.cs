c72285830a645f9960189bf8866aaec5af51420e
Am5ters <amyh968@gmail.com>
2024-05-01 15:08:21 -0500

    Custom Attributes
    Sliders and Tooltips

--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -5,18 +5,33 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class FPSController : MonoBehaviour
 {
+    [Tooltip("Insert player camera here")]
     public Camera playerCamera;
+
+    [Header("Movement Speed")]
+    [SerializeField][Range(1,50)]
     public float walkSpeed = 6f;
+    [SerializeField][Range(1,100)]
     public float runSpeed = 12f;
+
+    [Header("Weeeee")]
+    [SerializeField][Range(0,100)]
+    [Tooltip("How strong are your legs?")]
     public float jumpPower = 7f;
+    [SerializeField][Range(0,100)]
+    [Tooltip("How strong is gravity?")]
     public float gravity = 10f;
 
+    [Header("Camera Movement")]
+    [Tooltip("How fast you can turn your head")]
     public float lookSpeed = 2f;
+    [Tooltip("How far you can turn your head")]
     public float lookXLimit = 45f;
 
     Vector3 moveDirection = Vector3.zero;
     float rotationX = 0;
 
+    [Header("Can I move?")]
     public bool canMove = true;
 
     CharacterController characterController;
